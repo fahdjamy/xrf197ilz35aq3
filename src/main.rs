7b0a415616706260458edb43538bb891010a91df
fahdjamy <fahdjamy2@gmail.com>
2025-04-03 17:39:37 -0500

    Add tracing package

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use anyhow::anyhow;
+use tracing::{error, info};
 use xrfq3::{load_config, setup_tracing_logger};
 
 fn main() -> anyhow::Result<()> {
     let config = load_config().map_err(|err| {
+        error!("Failed to load configs, err={}", err);
         anyhow!("Failed to load configuration: {}", err)
     })?;
 
     let _guard = setup_tracing_logger(&config.app.name, &config.log);
 
-    println!("!!!xrf197ilz35aq3!!!");
+    info!("!!!xrf197ilz35aq3!!!");
 
     Ok(())
 }
