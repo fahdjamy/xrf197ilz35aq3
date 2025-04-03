efefb26774adb73f7538ee8be3328377009f97bc
fahdjamy <fahdjamy2@gmail.com>
2025-04-03 17:34:45 -0500

    Use telemetry in the main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use anyhow::anyhow;
+use xrfq3::{load_config, setup_tracing_logger};
+
+fn main() -> anyhow::Result<()> {
+    let config = load_config().map_err(|err| {
+        anyhow!("Failed to load configuration: {}", err)
+    })?;
+
+    let _guard = setup_tracing_logger(&config.app.name, &config.log);
+
     println!("!!!xrf197ilz35aq3!!!");
+
+    Ok(())
 }
