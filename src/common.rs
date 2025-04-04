8210f25a76637cb3f398c4270ec211f834c287e8
fahdjamy <fahdjamy2@gmail.com>
2025-04-03 23:24:33 -0500

    Change how request ids are generated

--- src/common.rs
+++ src/common.rs
@@ -1,8 +1,10 @@
 use uuid::Uuid;
+use chrono;
 
 pub struct RequestId(String);
 
 pub fn generate_request_id() -> String {
     // Replace with a more robust generator like UUID if preferred
-    format!("xrf_ilz_q3_{}", Uuid::new_v4().to_string())
+    let time_ms = chrono::Utc::now().timestamp_millis();
+    format!("xrf_ilz_q3_{}*{}", Uuid::new_v4().to_string(), time_ms)
 }
