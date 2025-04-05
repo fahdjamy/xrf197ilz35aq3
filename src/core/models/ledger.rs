8712e723c1fe3036b3b3be026da81c9b8ab9ca5f
fahdjamy <fahdjamy2@gmail.com>
2025-04-04 19:17:11 -0500

    Refactor amount field and make it decimal

--- src/core/models/ledger.rs
+++ src/core/models/ledger.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Utc};
+use rust_decimal::Decimal;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum EntryType {
@@ -10,11 +11,34 @@ pub enum EntryType {
 pub struct LedgerEntry {
     pub id: String,
     pub block: String,
-    pub amount: String,
+    pub amount: Decimal,
     pub account_id: String,
     pub entry_type: EntryType,
     pub transaction_id: String,
     pub sequence_number: String,
     pub timestamp: DateTime<Utc>,
     pub description: Option<String>,
+}
+
+impl LedgerEntry {
+    pub fn new(
+        block: String,
+        amount: Decimal,
+        account_id: String,
+        desc: Option<String>,
+        entry_type: EntryType,
+        transaction_id: String,
+    ) -> Self {
+        LedgerEntry {
+            block,
+            amount,
+            account_id,
+            entry_type,
+            transaction_id,
+            description: desc,
+            id: "".to_string(),
+            timestamp: Utc::now(),
+            sequence_number: "".to_string(),
+        }
+    }
 }
--- src/core/models/transaction.rs
+++ src/core/models/transaction.rs
@@ -1,10 +1,18 @@
 use chrono::{DateTime, Utc};
+use rust_decimal::Decimal;
 use serde::Serialize;
 
 enum TransactionType {
     Payment,
+    Transfer,
+    Reversal,
+    Commission,
+    Correction,
 }
 
+/// Once a Transaction is Completed, its associated LedgerEntry records should never be changed or deleted.
+/// Corrections should be made via new transactions (e.g., a Reversal or Correction transaction type)
+/// that create new offsetting LedgerEntry records. Enforce this through app logic & DB permissions
 #[derive(Debug, Clone, Serialize)]
 enum TransactionStatus {
     Failed,
@@ -16,15 +24,15 @@ enum TransactionStatus {
 
 pub struct Transaction {
     pub id: String,
-    pub amount: u64,
+    pub amount: Decimal,
     pub account_id: String,
     pub timestamp: DateTime<Utc>,
     pub status: TransactionStatus,
-    transaction_type: TransactionType,
+    pub transaction_type: TransactionType,
 }
 
 impl Transaction {
-    pub fn new(amount: u64, account_id: String) -> Self {
+    pub fn new(amount: Decimal, account_id: String) -> Self {
         Transaction {
             amount,
             account_id,
