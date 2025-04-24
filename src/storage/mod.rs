d5aa11dbc7cbb6d4af38092192a23ba929431495
fahdjamy <fahdjamy2@gmail.com>
2025-04-24 16:15:01 -0500

    Add a method to set up a timescale DB pool connection

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,2 +1,4 @@
 mod postgres;
 mod timescale;
+
+pub use timescale::setup_timescale_db;
--- src/storage/timescale.rs
+++ src/storage/timescale.rs
@@ -1,32 +1,41 @@
 use crate::{DatabaseConfig, PostgresConfig};
-use anyhow::Context;
 use sqlx::postgres::PgPoolOptions;
-use std::sync::OnceLock;
+use sqlx::PgPool;
+use std::time::Duration;
+use tokio::sync::OnceCell;
 
-static INIT_TIMESCALE_DB: OnceLock<()> = OnceLock::new();
+static INIT_TIMESCALE_DB_POOL: OnceCell<PgPool> = OnceCell::const_new();
 
-pub async fn setup_timescale_db(database_config: DatabaseConfig) {
-    INIT_TIMESCALE_DB.call_once(async || {
-        let timescale_config = database_config.timescale;
-        let pg_config = PostgresConfig::new(
-            timescale_config.port,
-            timescale_config.host.clone(),
-            timescale_config.name.clone(),
-            timescale_config.username.clone(),
-            timescale_config.require_ssl,
-            timescale_config.password.clone(),
-        );
+pub async fn setup_timescale_db(
+    database_config: DatabaseConfig,
+) -> Result<&'static PgPool, anyhow::Error> {
+    INIT_TIMESCALE_DB_POOL
+        .get_or_try_init(async || Ok(get_timescale_db_pool(database_config).await))
+        .await
+}
 
-        let pg_pool = PgPoolOptions::new()
-            .max_connections(timescale_config.max_conn as u32)
-            .connect_lazy_with(pg_config.connect_to_database(&pg_config.name));
+async fn get_timescale_db_pool(db_config: DatabaseConfig) -> PgPool {
+    let timescale_config = db_config.timescale;
+    let pg_config = PostgresConfig::new(
+        timescale_config.port,
+        timescale_config.host.clone(),
+        timescale_config.name.clone(),
+        timescale_config.username.clone(),
+        timescale_config.require_ssl,
+        timescale_config.password.clone(),
+    );
 
-        // --- TimescaleDB Specific Setup ---
+    let pg_pool = PgPoolOptions::new()
+        .max_connections(timescale_config.max_conn as u32)
+        .acquire_timeout(Duration::from_secs(5))
+        .connect_lazy_with(pg_config.connect_to_database(&pg_config.name));
 
-        // 1. Ensure TimescaleDB extension is enabled (often automatic in Docker image)
-        sqlx::query("CREATE EXTENSION IF NOT EXISTS timescaledb;")
-            .execute(&pg_pool)
-            .await
-            .context("Failed to create timescaledb table".to_string())?;
-    });
+    // --- TimescaleDB Specific Setup ---
+    // 1. Ensure TimescaleDB extension is enabled (often automatic in Docker image)
+    sqlx::query("CREATE EXTENSION IF NOT EXISTS timescaledb;")
+        .execute(&pg_pool)
+        .await
+        .expect("Failed to create TimescaleDB extension");
+
+    pg_pool
 }
