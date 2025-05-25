7e54712c950958de6bea783d7a65a321f9f1cdfe
fahdjamy <fahdjamy2@gmail.com>
2025-05-25 14:50:41 -0500

    add get_wallet_holding to fetch a wallet holding

--- src/storage/postgres/wallet.rs
+++ src/storage/postgres/wallet.rs
@@ -32,3 +32,26 @@ VALUES ($1, $2, $3, $4)",
 
     Ok(result.rows_affected() == 1)
 }
+
+#[tracing::instrument(
+    level = "debug",
+    skip(pg_pool, account_id),
+    name = "fetch account balance"
+)]
+pub async fn get_wallet_holding<'a, E>(
+    pg_pool: E,
+    account_id: &str,
+) -> Result<WalletHolding, PgDatabaseError>
+where
+    E: Executor<'a, Database = Postgres>,
+{
+    let result = sqlx::query_as!(
+        WalletHolding,
+        "SELECT balance, account_id, last_entry_id, modification_time FROM wallet WHERE account_id = $1",
+        account_id
+    )
+    .fetch_one(pg_pool)
+    .await?;
+
+    Ok(result)
+}
