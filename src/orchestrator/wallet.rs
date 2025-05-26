fce597e49b4571848d71d1830d09a36154850399
fahdjamy <fahdjamy2@gmail.com>
2025-05-26 08:11:30 -0500

    Add 2 methods (get_wallet_holding and credit_wallet_holding) to be used to credit the wallet's balance

--- src/orchestrator/wallet.rs
+++ src/orchestrator/wallet.rs
@@ -1,7 +1,10 @@
 use crate::core::WalletHolding;
 use crate::error::OrchestrateError;
-use crate::storage::create_wallet;
-use sqlx::{Executor, Postgres};
+use crate::storage::{create_wallet, fetch_wallet, update_wallet_balance};
+use chrono::Utc;
+use rust_decimal::prelude::Zero;
+use rust_decimal::Decimal;
+use sqlx::{Executor, PgPool, Postgres};
 
 pub async fn create_wallet_holding<'a, E>(
     pool: E,
@@ -14,9 +17,56 @@ where
     let wallet_holding = WalletHolding::new(acct_id, ledger_entry_id);
 
     let wallet_created = create_wallet(pool, &wallet_holding).await?;
-    if !!wallet_created {
+    if !wallet_created {
         return Ok(None);
     }
 
     Ok(Some(wallet_holding))
+}
+
+pub async fn get_wallet_holding<'a, E>(
+    pool: E,
+    acct_id: String,
+) -> Result<Option<WalletHolding>, OrchestrateError>
+where
+    E: Executor<'a, Database = Postgres>,
+{
+    let wallet_holding = fetch_wallet(pool, &acct_id).await?;
+
+    Ok(Some(wallet_holding))
+}
+
+pub async fn credit_wallet_holding<'a, E>(
+    pool: &PgPool,
+    amount: Decimal,
+    acct_id: String,
+    ledger_entry_id: String,
+) -> Result<bool, OrchestrateError>
+where
+    E: Executor<'a, Database = Postgres> + Clone,
+{
+    if amount == Decimal::zero() {
+        return Err(OrchestrateError::InvalidArgument(
+            "Amount cannot be zero".to_string(),
+        ));
+    }
+    let mut wallet_holding = match get_wallet_holding(pool, acct_id).await? {
+        Some(wallet_holding) => wallet_holding,
+        None => {
+            return Err(OrchestrateError::NotFoundError(
+                "No wallet for account found".to_string(),
+            ))
+        }
+    };
+
+    wallet_holding.modification_time = Utc::now();
+    wallet_holding.last_entry_id = Some(ledger_entry_id);
+    wallet_holding.balance = wallet_holding.balance + amount;
+
+    let updated_wallet = update_wallet_balance(pool, &wallet_holding).await?;
+
+    if updated_wallet.balance != wallet_holding.balance {
+        return Ok(false);
+    }
+    Ok(true)
 }
