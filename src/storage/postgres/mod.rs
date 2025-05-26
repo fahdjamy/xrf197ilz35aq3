a815ebd3ca5ad438ea7402ed9c16c220ba91e425
fahdjamy <fahdjamy2@gmail.com>
2025-05-26 07:10:47 -0500

    Add a method to update a wallet holding data

--- src/storage/postgres/mod.rs
+++ src/storage/postgres/mod.rs
@@ -8,4 +8,4 @@ pub use account::save_account;
 pub use chain::save_chain_stamp;
 pub use initialize::setup_postgres;
 pub use ledger::save_ledger;
-pub use wallet::{create_wallet, get_wallet_holding};
+pub use wallet::{create_wallet, fetch_wallet, update_wallet_balance};
--- src/storage/postgres/wallet.rs
+++ src/storage/postgres/wallet.rs
@@ -38,7 +38,7 @@ VALUES ($1, $2, $3, $4)",
     skip(pg_pool, account_id),
     name = "fetch account balance"
 )]
-pub async fn get_wallet_holding<'a, E>(
+pub async fn fetch_wallet<'a, E>(
     pg_pool: E,
     account_id: &str,
 ) -> Result<WalletHolding, PgDatabaseError>
@@ -49,6 +49,34 @@ where
         WalletHolding,
         "SELECT balance, account_id, last_entry_id, modification_time FROM wallet WHERE account_id = $1",
         account_id
+    )
+    .fetch_one(pg_pool)
+    .await?;
+
+    Ok(result)
+}
+
+#[tracing::instrument(
+    level = "debug",
+    skip(pg_pool, holding),
+    name = "update wallet holding information"
+)]
+pub async fn update_wallet_balance<'a, E>(
+    pg_pool: E,
+    holding: &WalletHolding,
+) -> Result<WalletHolding, PgDatabaseError>
+where
+    E: Executor<'a, Database = Postgres>,
+{
+    let result = sqlx::query_as!(
+        WalletHolding,
+        r#"UPDATE wallet SET balance = $1, last_entry_id = $2, modification_time = $3
+              WHERE account_id = $4
+              RETURNING balance, last_entry_id, modification_time, account_id"#,
+        holding.balance as Decimal,
+        holding.last_entry_id,
+        holding.modification_time,
+        holding.account_id,
     )
     .fetch_one(pg_pool)
     .await?;
