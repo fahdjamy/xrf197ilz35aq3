2b967dfc0fe354765cf84761637fedd05e7b700a
fahdjamy <fahdjamy2@gmail.com>
2025-05-27 23:23:13 -0500

    Add method to debit wallet balance

--- src/orchestrator/mod.rs
+++ src/orchestrator/mod.rs
@@ -10,4 +10,4 @@ pub use block::create_block;
 pub use chain::create_chain_stamp;
 pub use ledger::create_ledger;
 pub use transaction::create_payment_transaction;
-pub use wallet::{create_wallet_holding, credit_wallet_holding, get_wallet_holding};
+pub use wallet::{create_wallet_holding, credit_wallet_holding, debit_wallet, get_wallet_holding};
--- src/orchestrator/wallet.rs
+++ src/orchestrator/wallet.rs
@@ -36,15 +36,12 @@ where
     Ok(Some(wallet_holding))
 }
 
-pub async fn credit_wallet_holding<'a, E>(
+pub async fn credit_wallet_holding(
     pool: &PgPool,
     amount: Decimal,
     acct_id: String,
     ledger_entry_id: String,
-) -> Result<bool, OrchestrateError>
-where
-    E: Executor<'a, Database = Postgres> + Clone,
-{
+) -> Result<bool, OrchestrateError> {
     if amount == Decimal::zero() {
         return Err(OrchestrateError::InvalidArgument(
             "Amount cannot be zero".to_string(),
@@ -65,6 +62,44 @@ where
 
     let updated_wallet = update_wallet_balance(pool, &wallet_holding).await?;
 
+    if updated_wallet.balance != wallet_holding.balance {
+        return Ok(false);
+    }
+    Ok(true)
+}
+
+pub async fn debit_wallet(
+    pool: &PgPool,
+    amount: Decimal,
+    acct_id: String,
+    ledger_entry_id: String,
+) -> Result<bool, OrchestrateError> {
+    if amount == Decimal::zero() {
+        return Err(OrchestrateError::InvalidArgument(
+            "Amount cannot be zero".to_string(),
+        ));
+    };
+
+    let mut wallet_holding = match get_wallet_holding(pool, acct_id).await? {
+        None => {
+            return Err(OrchestrateError::NotFoundError(
+                "No wallet for account found".to_string(),
+            ))
+        }
+        Some(wallet_h) => wallet_h,
+    };
+
+    if wallet_holding.balance < amount {
+        return Err(OrchestrateError::InvalidArgument(
+            "debit amount is higher than the balance".to_string(),
+        ));
+    }
+
+    wallet_holding.modification_time = Utc::now();
+    wallet_holding.last_entry_id = Some(ledger_entry_id);
+    wallet_holding.balance = wallet_holding.balance - amount;
+
+    let updated_wallet = update_wallet_balance(pool, &wallet_holding).await?;
     if updated_wallet.balance != wallet_holding.balance {
         return Ok(false);
     }
