545cd7840f240e3a35a58e4a4906be8f5a2ab95e
fahdjamy <fahdjamy2@gmail.com>
2025-05-29 19:32:44 -0500

    move activity save logic from transaction file to activity file

--- src/storage/postgres/mod.rs
+++ src/storage/postgres/mod.rs
@@ -1,4 +1,5 @@
 mod account;
+mod activity;
 mod chain;
 mod initialize;
 mod ledger;
@@ -6,8 +7,8 @@ mod transactions;
 mod wallet;
 
 pub use account::save_account;
+pub use activity::save_activity;
 pub use chain::save_chain_stamp;
 pub use initialize::setup_postgres;
 pub use ledger::save_ledger;
-pub use transactions::save_activity;
 pub use wallet::{create_wallet, fetch_wallet, update_wallet_balance};
