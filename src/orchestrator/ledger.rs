2adcb0d4b220cdf8eb9df81c7a8a188d42e0689b
fahdjamy <fahdjamy2@gmail.com>
2025-06-05 17:16:47 -0500

    Refactor the create_ledger method

--- src/orchestrator/ledger.rs
+++ src/orchestrator/ledger.rs
@@ -2,28 +2,24 @@ use crate::core::{EntryType, LedgerEntry};
 use crate::error::OrchestrateError;
 use crate::storage::save_ledger;
 use sqlx::{Executor, Postgres};
-use std::str::FromStr;
 
 pub async fn create_ledger<'a, E>(
     pool: E,
-    entry: String,
-    acct_id: String,
+    entry: EntryType,
+    account_id: String,
     desc: Option<String>,
 ) -> Result<LedgerEntry, OrchestrateError>
 where
     E: Executor<'a, Database = Postgres>,
 {
-    let entry_type = EntryType::from_str(&entry)
-        .map_err(|err| OrchestrateError::InvalidArgument(err.to_string()))?;
-
-    let ledger = LedgerEntry::new(acct_id.clone(), desc, entry_type);
+    let ledger = LedgerEntry::new(account_id.clone(), desc, entry);
 
     // store ledger entry into the database
     let ledger_entry_created = save_ledger(pool, &ledger).await?;
     if !ledger_entry_created {
         return Err(OrchestrateError::ServerError(format!(
             "could not create a new ledger entry for acctId={}",
-            acct_id
+            account_id
         )));
     }
 
