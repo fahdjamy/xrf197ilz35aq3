e4589331cc6767bd888942e13c9ebe389ce616dc
fahdjamy <fahdjamy2@gmail.com>
2025-06-16 18:33:00 -0500

    Modify the wallet table and remove last_entry_id and add a currency column

--- migrations/0002_create_wallet_table.up.sql
+++ migrations/0002_create_wallet_table.up.sql
@@ -1,9 +1,10 @@
 -- Add up a migration script here
 CREATE TABLE IF NOT EXISTS wallet
 (
-    last_entry_id     VARCHAR(255)             NOT NULL,
+    currency          currency_enum            NOT NULL,
     balance           NUMERIC(18, 4)           NOT NULL,
     modification_time TIMESTAMP WITH TIME ZONE NOT NULL,
-    account_id        VARCHAR(255)             NOT NULL PRIMARY KEY,
+    account_id        VARCHAR(255)             NOT NULL,
+    PRIMARY KEY (account_id, currency),
     FOREIGN KEY (account_id) REFERENCES user_account (id) ON DELETE CASCADE
 )
