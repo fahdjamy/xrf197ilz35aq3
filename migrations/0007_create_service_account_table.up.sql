27e7c3b50573ee7ac20a08608b609293e6dfb865
fahdjamy <fahdjamy2@gmail.com>
2025-07-14 16:38:29 -0500

    Add sql migrate tables

---
+++ migrations/0007_create_service_account_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS service_account
+(
+    id                VARCHAR(255)             NOT NULL PRIMARY KEY,
+    locked            BOOLEAN                  NOT NULL DEFAULT FALSE,
+    app_id            VARCHAR(255)             NOT NULL,
+    creation_time     TIMESTAMP WITH TIME ZONE NOT NULL,
+    modification_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    admin_user_fps    VARCHAR(255)[],
+    holders_user_fps  VARCHAR(255)[],
+    status            account_status           NOT NULL DEFAULT 'Active',
+    acct_type         account_type             NOT NULL DEFAULT 'SystemFee',
+
+    -- This constraint ensures the 'admin_user_fps' & 'holders_user_fps array must have at least one element.
+    -- cardinality() returns the total number of elements in an array.
+    CONSTRAINT admin_user_fps_must_not_be_empty CHECK (cardinality(admin_user_fps) > 0),
+    CONSTRAINT holders_user_fps_must_not_be_empty CHECK (cardinality(holders_user_fps) > 0)
+)
