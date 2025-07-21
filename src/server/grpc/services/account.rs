d0cc9386846cc297a4e0722e6e427b169417ed97
fahdjamy <fahdjamy2@gmail.com>
2025-07-21 17:48:11 -0500

    Remove acct_type from WalletResponse gRPC response

--- src/server/grpc/services/account.rs
+++ src/server/grpc/services/account.rs
@@ -1,15 +1,16 @@
 use crate::context::{ApplicationContext, UserContext};
 use crate::error::OrchestrateError;
 use crate::grpc_services::account_service_server::AccountService;
-use crate::grpc_services::{CreateAccountRequest, CreateAccountResponse};
+use crate::grpc_services::{CreateAccountRequest, CreateAccountResponse, WalletResponse};
 use crate::server::grpc::header::get_xrf_user_auth_header;
 use crate::server::grpc::interceptors::trace_request;
 use crate::{create_account, generate_request_id, REQUEST_ID_KEY, XRF_USER_FINGERPRINT};
 use cassandra_cpp::Session;
+use prost_types::Timestamp;
 use sqlx::PgPool;
 use std::sync::Arc;
 use tonic::{Request, Response, Status};
-use tracing::{info, info_span};
+use tracing::{info, info_span, warn};
 
 pub struct AccountServiceManager {
     pg_pool: Arc<PgPool>,
@@ -47,7 +48,7 @@ impl AccountService for AccountServiceManager {
         );
         let user_ctx = UserContext::load_user_context(user_fp, req.timezone.clone(), None, None);
 
-        let _ = create_account(
+        let (created_acct, created_wallet) = create_account(
             &self.pg_pool,
             req.currency,
             req.acct_type,
@@ -66,6 +67,30 @@ impl AccountService for AccountServiceManager {
             _ => Status::internal("Internal server error"),
         })?;
 
-        unimplemented!()
+        Ok(Response::new(CreateAccountResponse {
+            locked: created_acct.locked,
+            status: created_acct.status.to_string(),
+            account_id: created_acct.id.to_string(),
+            account_type: created_acct.account_type.to_string(),
+            creation_time: Some(Timestamp {
+                seconds: created_acct.creation_time.timestamp(),
+                nanos: created_acct.creation_time.timestamp_subsec_nanos() as i32,
+            }),
+            modification_time: Some(Timestamp {
+                seconds: created_acct.modification_time.timestamp(),
+                nanos: created_acct.modification_time.timestamp_subsec_nanos() as i32,
+            }),
+            wallet_holding: Some(WalletResponse {
+                balance: f32::try_from(created_wallet.balance).unwrap_or_else(|er| {
+                    warn!("Err converting balance : err={}, defaulted to 0.0", er);
+                    0.0
+                }),
+                currency: created_wallet.currency.to_string(),
+                modification_time: Some(Timestamp {
+                    seconds: created_wallet.modification_time.timestamp(),
+                    nanos: created_wallet.modification_time.timestamp_subsec_nanos() as i32,
+                }),
+            }),
+        }))
     }
 }
