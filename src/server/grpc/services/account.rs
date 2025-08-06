7aedc11a36323d1d50923d24d19afbba4982e2c6
fahdjamy <fahdjamy2@gmail.com>
2025-08-05 19:38:57 -0500

    Add temporary implementations for the added gRPC methods

--- src/server/grpc/services/account.rs
+++ src/server/grpc/services/account.rs
@@ -1,7 +1,11 @@
 use crate::context::{ApplicationContext, UserContext};
 use crate::error::OrchestrateError;
 use crate::grpc_services::account_service_server::AccountService;
-use crate::grpc_services::{CreateAccountRequest, CreateAccountResponse, WalletResponse};
+use crate::grpc_services::{
+    AccountResponse, CreateAccountRequest, CreateAccountResponse, GetUserAccountRequest,
+    GetUserAccountResponse, GetUserAccountsRequest, GetUserAccountsResponse,
+    GetWalletHoldingRequest, GetWalletHoldingResponse, WalletResponse,
+};
 use crate::server::grpc::header::get_xrf_user_auth_header;
 use crate::server::grpc::interceptors::trace_request;
 use crate::{create_account, generate_request_id, REQUEST_ID_KEY, XRF_USER_FINGERPRINT};
@@ -68,29 +72,52 @@ impl AccountService for AccountServiceManager {
         })?;
 
         Ok(Response::new(CreateAccountResponse {
-            locked: created_acct.locked,
-            status: created_acct.status.to_string(),
-            account_id: created_acct.id.to_string(),
-            account_type: created_acct.account_type.to_string(),
-            creation_time: Some(Timestamp {
-                seconds: created_acct.creation_time.timestamp(),
-                nanos: created_acct.creation_time.timestamp_subsec_nanos() as i32,
-            }),
-            modification_time: Some(Timestamp {
-                seconds: created_acct.modification_time.timestamp(),
-                nanos: created_acct.modification_time.timestamp_subsec_nanos() as i32,
-            }),
-            wallet_holding: Some(WalletResponse {
-                balance: f32::try_from(created_wallet.balance).unwrap_or_else(|er| {
-                    warn!("Err converting balance : err={}, defaulted to 0.0", er);
-                    0.0
+            account: Some(AccountResponse {
+                locked: created_acct.locked,
+                status: created_acct.status.to_string(),
+                account_id: created_acct.id.to_string(),
+                account_type: created_acct.account_type.to_string(),
+                creation_time: Some(Timestamp {
+                    seconds: created_acct.creation_time.timestamp(),
+                    nanos: created_acct.creation_time.timestamp_subsec_nanos() as i32,
                 }),
-                currency: created_wallet.currency.to_string(),
                 modification_time: Some(Timestamp {
-                    seconds: created_wallet.modification_time.timestamp(),
-                    nanos: created_wallet.modification_time.timestamp_subsec_nanos() as i32,
+                    seconds: created_acct.modification_time.timestamp(),
+                    nanos: created_acct.modification_time.timestamp_subsec_nanos() as i32,
+                }),
+                wallet_holding: Some(WalletResponse {
+                    balance: f32::try_from(created_wallet.balance).unwrap_or_else(|er| {
+                        warn!("Err converting balance : err={}, defaulted to 0.0", er);
+                        0.0
+                    }),
+                    currency: created_wallet.currency.to_string(),
+                    modification_time: Some(Timestamp {
+                        seconds: created_wallet.modification_time.timestamp(),
+                        nanos: created_wallet.modification_time.timestamp_subsec_nanos() as i32,
+                    }),
                 }),
             }),
         }))
+    }
+
+    async fn get_user_account(
+        &self,
+        request: Request<GetUserAccountRequest>,
+    ) -> Result<Response<GetUserAccountResponse>, Status> {
+        unimplemented!()
+    }
+
+    async fn get_user_accounts(
+        &self,
+        request: Request<GetUserAccountsRequest>,
+    ) -> Result<Response<GetUserAccountsResponse>, Status> {
+        unimplemented!()
+    }
+
+    async fn get_wallet_holding(
+        &self,
+        request: Request<GetWalletHoldingRequest>,
+    ) -> Result<Response<GetWalletHoldingResponse>, Status> {
+        unimplemented!()
     }
 }
